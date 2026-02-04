7714de4ada6f0cff3f9c013631031514ec833ae5
Igor Zaworski <igor.zaworski2@gmail.com>
2026-02-04 17:23:21 +0100

    Introduction of Jazzy support (#2)
    Signed-off-by: igorosky <igor.zaworski2@gmail.com>

--- include/scorpio_utils/threading/thread_pool.hpp
+++ include/scorpio_utils/threading/thread_pool.hpp
@@ -13,7 +13,7 @@ constexpr void scorpio_utils::threading::ThreadPool::async_call(
   static_assert(std::is_invocable_v<Fn>, "Task must be invocable");
   static_assert(std::is_same_v<Return, std::invoke_result_t<Fn>>,
                 "Return type must match the task's return type");
-  add_task(std::move([this, task = std::move(task), future = std::move(future)]() mutable {
+  add_task([this, task = std::move(task), future = std::move(future)]() mutable {
       future->set_started();
       if constexpr (std::is_same_v<Return, void>) {
         task();
@@ -21,7 +21,7 @@ constexpr void scorpio_utils::threading::ThreadPool::async_call(
       } else {
         future->set_done(task());
       }
-   }));
+   });
 }
 
 template<typename Fn, bool Wait>
@@ -64,7 +64,7 @@ constexpr scorpio_utils::threading::WaitGroup::shared_ptr scorpio_utils::threadi
   WaitGroup::shared_ptr wait_group = std::make_shared<WaitGroup>();
   for (; begin != end; ++begin) {
     const auto future = async([task, begin = *begin]() mutable { task(begin); });
-    wait_group->add(std::move(std::static_pointer_cast<WaitableBase>(future)));
+    wait_group->add(std::static_pointer_cast<WaitableBase>(future));
   }
   return wait_group;
 }
@@ -89,7 +89,7 @@ constexpr scorpio_utils::threading::WaitGroup::shared_ptr scorpio_utils::threadi
             task(static_cast<N>(begin + i));
           }
     });
-    wait_group->add(std::move(std::static_pointer_cast<WaitableBase>(future)));
+    wait_group->add(std::static_pointer_cast<WaitableBase>(future));
     begin += static_cast<N>(work_per_thread + 1);
   }
   for (; begin < end; begin += work_per_thread) {
@@ -98,7 +98,7 @@ constexpr scorpio_utils::threading::WaitGroup::shared_ptr scorpio_utils::threadi
             task(static_cast<N>(begin + i));
           }
     });
-    wait_group->add(std::move(std::static_pointer_cast<WaitableBase>(future)));
+    wait_group->add(std::static_pointer_cast<WaitableBase>(future));
   }
   return wait_group;
 }
