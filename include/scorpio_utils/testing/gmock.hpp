8b140768637a6555841ee587ca407be16d64024e
igorosky <igor.zaworski2@gmail.com>
2026-02-05 17:13:24 +0100

    Introduction of gtest & ScorpioUdp::close() fix
    Signed-off-by: igorosky <igor.zaworski2@gmail.com>

---
+++ include/scorpio_utils/testing/gmock.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wdeprecated"
+#include <gmock/gmock.h>
+#pragma GCC diagnostic pop
