484c2c705f85ff1bc1873deadd9cf9cdb46c674d
Mariusz Rychlicki <mariuszrychlicki04@gmail.com>
2026-05-14 12:32:50 +0200

    Introduction of ros logger
    Signed-off-by: Mariusz Rychlicki <mariuszrychlicki04@gmail.com>

---
+++ include/scorpio_utils/logger/ros_logger.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include "scorpio_utils/logger/logger.hpp"
+
+namespace scorpio_utils::logger {
+class ROSLogger : public Logger {
+public:
+  explicit ROSLogger(rclcpp::Logger logger)
+  : _logger(std::move(logger)) {}
+
+  void log(Level level, std::string&& message) override {
+    switch (level) {
+      case Level::FATAL:
+      case Level::ERROR:   RCLCPP_ERROR(_logger, "%s", message.c_str()); break;
+      case Level::WARNING: RCLCPP_WARN (_logger, "%s", message.c_str()); break;
+      case Level::INFO:    RCLCPP_INFO (_logger, "%s", message.c_str()); break;
+      case Level::DEBUG:
+      case Level::TRACE:   RCLCPP_DEBUG(_logger, "%s", message.c_str()); break;
+    }
+  }
+
+private:
+  rclcpp::Logger _logger;
+};
+}  // namespace scorpio_utils::logger
